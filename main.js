aed259a7bfe7ab42892b74f2210ace744605acf2
GabrielMaimoRigo <bielmaimo16@gmail.com>
2022-05-28 13:15:43 +0200

    atf dispositivo ordenador completo

--- main.js
+++ main.js
@@ -4,14 +4,38 @@
             if (window.scrollY > 350) {
                 document.body.classList.add('hero_animation')
             } else {
-                document.body.classList.remove('hero_animation')  
+                document.body.classList.remove('hero_animation')
             }
         })
 
         // side login bar animation
-     
-        /* let login = document.querySelector('#toggleMenu');
-        let sideBar = document.querySelector('#loginSideBar');
-        login.addEventListener('click', () => {
-            sideBar.classList.toggle('active')
-        }) */
+
+        let register = document.querySelector('#toggleRegister');
+        let sideBar = document.querySelector('#registerSidebar');
+        register.addEventListener('click', (e) => {
+            e.preventDefault();
+            sideBar.classList.add('active')
+        })
+
+        let closeSidebar = document.querySelector('#closeSidebar');
+        closeSidebar.addEventListener('click', (e) => {
+            e.preventDefault();
+            sideBar.classList.remove('active')
+        })
+
+        //Primer uso de biblioteca gsap
+        gsap.to(".hero_title", {x: 300, duration: 1.5, opacity: 1, ease: Power3.easeOut});
+
+        gsap.to(".hero_description", {x: 300, duration: 1.5, opacity: 1, delay: 0.8,ease: Power3.easeOut});
+
+        gsap.to(".hero_logo", {y: 20, duration: 1.5, opacity: 1, ease: Power3.easeOut})
+
+        gsap.to(".hero_login", {y: 20, duration: 1.5, opacity: 1, ease: Power3.easeOut})
+
+        gsap.from(".hero_items li", {
+            opacity: 0, 
+            y: 20, 
+            duration: 1.5,
+            delay: 1,
+            stagger: 0.3
+        });
